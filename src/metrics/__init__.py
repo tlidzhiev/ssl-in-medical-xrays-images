f743b33a8e9b363949ee2c8bb18948f83dcf5011
Andrei3223 <a.gruzitski@gmail.com>
2024-11-30 14:58:00 +0300

    clone template

---
+++ src/metrics/__init__.py
@@ -0,0 +1,1 @@
+from src.metrics.accuracy import AccuracyMetric
---
+++ src/model/__init__.py
@@ -0,0 +1,4 @@
+from src.model.baseline_model import BaselineModel
+from src.model.resnet import ResNetModel
+
+__all__ = ["BaselineModel", "ResNetModel"]
